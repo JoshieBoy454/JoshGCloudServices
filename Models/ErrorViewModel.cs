af18f38f8a861b72c891642364a8942b619d83c0
Joshu <Joshu@DESKTOP-RFMJFA6>
2024-04-02 18:35:58 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JoshGCloudServices.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
