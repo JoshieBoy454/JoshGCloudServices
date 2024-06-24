f600b6fc97653788e44b430506cf151da96e9ec0
Joshua Gain <ST10369044@vcconnect.edu.za>
2024-06-24 22:01:15 +0200

    implemented search functions

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -1,5 +1,6 @@
 ﻿using JoshGCloudServices.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Data.SqlClient;
 
 namespace JoshGCloudServices.Controllers
@@ -39,7 +40,44 @@ namespace JoshGCloudServices.Controllers
             }
             catch (Exception)
             {
-               
+                return View("Error");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult UpdateTransaction(int transactionID, int userID, int productID)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Products.con_string))
+                {
+                    string sql = "UPDATE Transactions SET UserID = @UserID, ProductID = @ProductID WHERE TransactionID = @TransactionID";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, con))
+                    {
+                        cmd.Parameters.AddWithValue("@UserID", userID);
+                        cmd.Parameters.AddWithValue("@ProductID", productID);
+                        cmd.Parameters.AddWithValue("@TransactionID", transactionID);
+
+                        con.Open();
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        con.Close();
+
+                        if (rowsAffected > 0)
+                        {
+                            return RedirectToAction("MyWork", "Home");
+                        }
+                        else
+                        {
+                            return RedirectToAction("MyWork", "Home");
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
                 return View("Error");
             }
         }
